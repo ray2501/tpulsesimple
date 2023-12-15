f94ad6a6077fa07e65f896799cc2072c50d0b4d6
Danilo Chang <ray2501@gmail.com>
2023-12-15 10:51:11 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded tpulsesimple 0.3 \
-    [list load [file join $dir libtpulsesimple0.3.so] tpulsesimple]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded tpulsesimple 0.3 \
+	    [list load [file join $dir libtcl9tpulsesimple0.3.so] [string totitle tpulsesimple]]
+} else {
+    package ifneeded tpulsesimple 0.3 \
+	    [list load [file join $dir libtpulsesimple0.3.so] [string totitle tpulsesimple]]
+}
