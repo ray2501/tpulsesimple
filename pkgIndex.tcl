11a5f148b4e60cba972e8cae609bb26a3adc31e0
Danilo Chang <ray2501@gmail.com>
2025-01-29 13:16:37 +0800

    Bump version to 0.5

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded tpulsesimple 0.4 \
-	    [list load [file join $dir libtcl9tpulsesimple0.4.so] [string totitle tpulsesimple]]
+    package ifneeded tpulsesimple 0.5 \
+	    [list load [file join $dir libtcl9tpulsesimple0.5.so] [string totitle tpulsesimple]]
 } else {
-    package ifneeded tpulsesimple 0.4 \
-	    [list load [file join $dir libtpulsesimple0.4.so] [string totitle tpulsesimple]]
+    package ifneeded tpulsesimple 0.5 \
+	    [list load [file join $dir libtpulsesimple0.5.so] [string totitle tpulsesimple]]
 }
